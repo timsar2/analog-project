a7c80171a9b216afacb80c76559897f93eb519f4
timsar2 <info.golab@gmail.com>
2025-07-11 11:09:44 +0330

    initial commit

---
+++ src/app/pages/index.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import { AnalogWelcome } from './analog-welcome';
+
+@Component({
+  selector: 'app-home',
+  imports: [AnalogWelcome],
+  template: `
+     <app-analog-welcome/>
+  `,
+})
+export default class HomeComponent {
+}
